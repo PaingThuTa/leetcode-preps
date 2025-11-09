071c874227f770fdfbc1da5f9b3b534d95f3e8d0
PaingThuTa <paingthuta472@gmail.com>
2025-11-09 21:46:43 +0700

    remove duplicates prep

---
+++ easy/26_remove-duplicates-from-sorted-array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        pivot = 0
+        for i in range(1, len(nums)):
+            if nums[i] == nums[pivot]:
+                continue
+            else:
+                pivot += 1
+                nums[pivot] = nums[i]
+        return pivot + 1
+
+        
